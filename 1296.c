5399dbc89a843c5202961c56a866b5b239c4ed6a
huynhtancuong <huynhtancuong.hcm@gmail.com>
2023-02-09 12:09:32 +0300

    solved 1296

---
+++ 1296.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int n;
+    scanf("%d", &n);
+    int *arr;
+    arr = malloc(n*sizeof(int));
+    for (int i = 0; i< n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int max = 0;
+    for (int size = 2 ; size <= n; size++) {
+        for (int start = 0; start <= n-size; start++) {
+            int sum = 0;
+            for (int index = start; index < start+size; index++) {
+                sum+=arr[index];
+            }
+            // printf("sum= %d\n", sum);
+            if (max < sum) {
+                max = sum;
+            }
+        }
+    }
+
+    printf("%d", max);
+
+    free(arr);
+	return 0;
+}
