48d3d0893d9ef3c1c232e6d2ed47287114eb48dd
Huynh Tan Cuong <huynhtancuong.hcm@gmail.com>
2023-02-21 08:50:20 +0300

    cleaned 1207

--- 1207.c
+++ 1207.c
@@ -12,12 +12,7 @@ struct line
 struct point
 {
 	// point: (x, y)
-	int32_t x, y;
-};
-
-struct answer
-{
-	size_t p1, p2;
+	int64_t x, y;
 };
 
 
@@ -30,7 +25,8 @@ struct line get_line(struct point p1, struct point p2) {
 }
 
 float line_point_distance(struct line d, struct point M) {
-	float dis = (d.a*M.x + d.b*M.y + d.c)/(sqrt(d.a*d.a + d.b*d.b));
+	// float dis = (d.a*M.x + d.b*M.y + d.c)/(sqrt(d.a*d.a + d.b*d.b));
+	float dis = (d.a*M.x + d.b*M.y + d.c);
 	return dis;
 }
 
@@ -46,29 +42,6 @@ int is_equal_sets(struct point p[], size_t size, struct line d) {
 	return positive_counter == negative_counter;
 }
 
-size_t find_P(struct point ps[], size_t size, struct line d) {
-	size_t min_index;
-	float min_value = 10000000;
-	for (size_t i = 0; i<size; i++) {
-		float dis = line_point_distance(d, ps[i]);
-		if (dis < 0) dis = -dis;
-		if ((dis < min_value) && (dis > 0)) {
-			min_index = i;
-			min_value = dis;
-		}
-	}
-	return min_index;
-}
-
-struct answer find(size_t p1, size_t p2, struct point ps[], size_t size) {
-	struct line d = get_line(ps[p1], ps[p2]);
-	size_t p_index = find_P(ps, size, d);
-	struct line d1 = get_line(ps[p_index], ps[p1]);
-	struct line d2 = get_line(ps[p_index], ps[p2]);
-	if (is_equal_sets(ps, size, d1)) return (struct answer) {.p1=p_index, .p2=p1};
-	if (is_equal_sets(ps, size, d2)) return (struct answer) {.p1=p_index, .p2=p2};
-	return find(p1, p_index, ps, size);
-}
 
 
 int main() {
@@ -81,35 +54,18 @@ int main() {
 	points = (struct point *) malloc(sizeof(struct point)*N);
 
 	for (size_t i = 0; i<N; i++) {
-		scanf("%" SCNd32 " %" SCNd32, &points[i].x, &points[i].y);
+		scanf("%" SCNd64 " %" SCNd64, &points[i].x, &points[i].y);
 	}
 
-	struct point p1 = (struct point) {.x = 0, .y = 0};
-	struct point p2 = (struct point) {.x = 1, .y = 1};
-
-	struct line d = get_line(p1, p2);
-
-	// printf("%d", is_equal_sets(points, N, d));
-
-	// printf("P=%d", find_P(points, N, d));
-
-	struct answer ans;
-
-	ans = find(0, 1, points, N);
-
-	printf("%d %d", ans.p1 + 1, ans.p2 + 1);
-
-	// for (int i = 0; i< N; i++) {
-	// 	printf("%f\n", line_point_distance(d, points[i]));
-	// }
-
-	// printf("line d : %.2fx +  %.2fy + %.2f = 0\n", d.a, d.b, d.c);
-
-	// struct point p3 = (struct point) {.x = -1, .y = 0};
-
-	// float distance = line_point_distance(d, p3);
-
-	// printf("distance = %f", distance);
+	for (size_t i = 0; i<N; i++) {
+		for (size_t j=i+1; j<N; j++) {
+			struct line d = get_line(points[i], points[j]);
+			if (is_equal_sets(points, N, d)) {
+				printf("%d %d", i+1, j+1);
+				return 0;
+			}
+		}
+	}
 
 	return 0;
 }
