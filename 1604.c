83db7b3b704aa46a757bba5ea23c5300efa8adf8
Huynh Tan Cuong <huynhtancuong.hcm@gmail.com>
2023-02-26 15:15:48 +0300

    abit improve for 1604

--- 1604.c
+++ 1604.c
@@ -21,7 +21,7 @@ void bubblesort(Sign arr[], size_t n) {
                         }
 }
 
-Sign sign[100];
+Sign sign[10000];
 
 int main() {
         int n;
@@ -41,11 +41,11 @@ int main() {
                         printf("%d %d ", sign[0].index, sign[1].index);
                         sign[0].amount--;
                         sign[1].amount--;
+                        if (sign[1].amount == 0) bubblesort(sign, n);
                 } else {
                         printf("%d ", sign[0].index);
                         sign[0].amount--;
                 }
-                bubblesort(sign, n);
         }
 
 
