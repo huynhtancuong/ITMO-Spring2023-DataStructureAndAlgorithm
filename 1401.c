62eb2261487e28da976b595cc0ff159493982a6a
Huynh Tan Cuong <huynhtancuong.hcm@gmail.com>
2023-03-27 01:53:23 +0300

    added doc for 1401

--- 1401.c
+++ 1401.c
@@ -26,12 +26,24 @@ void print_map() {
 	printf("\n");
 }
 
+/**
+ * It fills a 2x2 square with numbers, except for the hole. 
+ * 
+ * If square is not the hole, then we fill the square with a number.
+ * We use i, j for interate for each square in the 2x2 square.
+ * 
+ * @param x the x coordinate of the top left corner of the square
+ * @param y the y coordinate of the top left corner of the square
+ * @param hx the x coordinate of the hole
+ * @param hy the y coordinate of the hole
+ */
 void fill_square_size_2(int x, int y, int hx, int hy) {
 	// we can check if the hole is not in this square, then do nothing
 	// but I don't :) 
 	for (int i=0; i<2; i++) {
 		for (int j=0; j<2; j++) {
-			if (!((x+i == hx) && (y+j == hy))) { // if not the hole, then fill
+			int is_the_hole = ((x+i == hx) && (y+j == hy));
+			if (!is_the_hole) { // if not the hole, then fill
 				map[x+i][y+j] = k++/3;
 			}
 		}
@@ -39,12 +51,28 @@ void fill_square_size_2(int x, int y, int hx, int hy) {
 	// print_map();
 }
 
+/**
+ * > The function recursively fills the square with size n, with the hole at (hx, hy), by filling the
+ * middle triangle, and recursively filling the 3 blocks not containing the hole
+ * 
+ * @param n the size of the square
+ * @param x the x coordinate of the top left corner of the square
+ * @param y the y coordinate of the top left corner of the square
+ * @param hx the x coordinate of the hole
+ * @param hy the y coordinate of the hole
+ * 
+ * @return the number of ways to fill a square of size n with a hole at (hx, hy).
+ */
 void solve(int n, int x, int y, int hx, int hy) {
+
+/* The simplest case. If the size of the square is 2, then we just fill the square with numbers, except
+for the hole. */
 	if (n == 2) {
 		fill_square_size_2(x, y, hx, hy); // simplest case
 		return;
 	}
 
+/* It finds the block that contains the hole. */
 	// let's find the (hi, hj) where the hole is 
 	int hi, hj;
 	for (int i=0; i<2; i++) {
@@ -54,11 +82,11 @@ void solve(int n, int x, int y, int hx, int hy) {
 		}
 	}
 
-	// fill the middle triagle 
+	/* It fills the middle triangle with numbers, except for the hole. */
 	int triangle_x = x + n/2-1, triangle_y = y + n/2-1;
 	fill_square_size_2(triangle_x, triangle_y, triangle_x + hi, triangle_y + hj);
 
-	// recursively fill the 3 blocks not contain the hole
+	/* Recursively filling the 3 blocks not contain the hole. */
 	for (int i=0; i<2; i++) {
 		for (int j=0; j<2; j++) {
 			if (!(i==hi && j==hj)) {// if not the block contain the hole
